cc99a134b59621eacfbdab8bffe6783e3a7c2e83
danielrojas76 <cdrojas.2018@gmail.com>
2023-12-01 10:11:20 -0300

    search pokemons

--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -1,4 +1,5 @@
-const pokeList = document.getElementById('pokemon-list')
+const pokeList = document.querySelector('#pokemon-list')
+const buscador = document.querySelector('#buscador')
 
 const fetchPokemons = async () => {
     const apiEndpoint = 'https://pokeapi.co/api/v2/pokemon/';
@@ -15,17 +16,55 @@ const fetchPokemons = async () => {
                 id: pokemonData.id
             }
 
-            // Crear las etiquetas HTML desde JS, y despues agregarlas al DOM (HTML)
+            // Creamos la etiqueta LI
             let pokemonLi = document.createElement('li');
-            pokemonLi.innerHTML = pokemon.name
+
+            //Le agregamos estilos, un ID unico y un innerHTML
+            pokemonLi.style = 'padding: 2%; margin: 2%; list-style-type: none;';
+            pokemonLi.id = `pokemon-${pokemon.id}`;
+            pokemonLi.classList.add('pokemon')
+
+            const pokemonImg = document.createElement('img')
+            pokemonImg.src = pokemon.img;
+            pokemonLi.appendChild(pokemonImg)
             
-            console.log(pokemonLi)
-            //pokeList.innerHTML += `<li>${pokemon.name}</li>`;
-     
+            const pokemonName = document.createElement('h2');
+            pokemonName.innerText = pokemon.name;
+            pokemonLi.appendChild(pokemonName);
+
+            const pokemonType = document.createElement('p');
+            pokemonType.innerText = pokemon.type;
+            pokemonLi.appendChild(pokemonType);
+            
+            //console.log(pokemonLi);
+            
+            //Al UL le agegamos cada LI como hijo
+            pokeList.appendChild(pokemonLi);
+
         } catch (error) {
             console.log(error)
         }
     }
+}
+
+buscador.addEventListener('input', e => {
+    searchPokemon(e.target.value);
+})
+
+const searchPokemon = (name) =>{
+    const pokemonList = Array.from(document.querySelectorAll('.pokemon h2'));
+
+    pokemonList.forEach(pokemonName =>{
+        // Si el texto del h2 no contiene la busqueda...
+        if(!pokemonName.innerText.includes(name)){
+            // Al padre del h2 actual le ponemos display none
+            pokemonName.parentElement.style.display = 'none';
+        } else {
+            pokemonName.parentElement.style.display = 'list-item';
+        }
+    })
+
+    //const filterList = pokemonList.filter(pokemon => pokemon.innerText.includes(name))
 }
 
 fetchPokemons()
